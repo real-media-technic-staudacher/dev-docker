e3143ca9c37533b42ef4a23bfcb086f6f561c12f
Patrick Thomas <github@patrick-thomas.de>
2018-02-06 13:28:44 +0100

    Fixed wrong settings

--- debian-apache-php70-mariadb/mariadb-10.1-testing/fix-permissions.sh
+++ debian-apache-php70-mariadb/mariadb-10.1-testing/fix-permissions.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+
 mysql -u root -e "grant all on *.* to '$MYSQL_USER'@'%' identified by '${MYSQL_PASSWORD}' with grant option; flush privileges;"
