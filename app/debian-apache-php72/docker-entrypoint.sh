fa64b4e4874bdb62fde4d470ed74a66297909c28
Patrick Thomas <github@patrick-thomas.de>
2018-02-22 18:31:11 +0100

    Allow to specify supervisor configurations within your docker-compose.yml.

--- app/debian-apache-php72/docker-entrypoint.sh
+++ app/debian-apache-php72/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 case "$1" in
         *sh)
@@ -7,6 +7,16 @@ case "$1" in
         ;;
 esac
 
-cp "${SUPERVISOR}" /etc/supervisor/conf.d
+########## Did only work directly in docker-entrypoint.sh ##############
+OLDIFS="$IFS"
+
+export IFS=" "
+
+for filename in $SUPERVISOR; do
+  cp "/var/www/html/resources/queue-worker/${filename}" /etc/supervisor/conf.d
+done
+
+export IFS="$OLDIFS"
+########## Did only work directly in docker-entrypoint.sh ##############
 
 exec /usr/bin/supervisord -n
